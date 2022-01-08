4c5797f79254c6a4bb238d5416f7e191fafa664c
George <s0fr0n@hotmail.com>
2022-01-08 14:04:46 +0200

    Initialization Commit v.1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+
+
+        <button class="btn btn-primary">daisyUI Button</button>
+    </div>
+  );
+}
+
+export default App;
